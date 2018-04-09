639d88e22d72fa8864f92936297532af12bf3d92
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-04-09 14:00:39 +0100

    Add problem6_spec.rb amnd tests

---
+++ spec/problem6_spec.rb
@@ -0,0 +1,14 @@
+require 'problem6'
+
+describe 'Problem6' do
+
+  describe 'Sum Square Difference' do
+    it 'Works Out The Sum Square Difference Of 10' do
+      expect(sum_square_difference(10)).to eq 2640
+    end
+
+    it 'Works Out The Sum Square Difference Of 100' do
+      expect(sum_square_difference(100)).to eq 25164150
+    end
+  end
+end
