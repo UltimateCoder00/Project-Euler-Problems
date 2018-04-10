3c306a86b787f2c67393136059d9282c045171d7
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-04-10 12:16:18 +0100

    Add problem25_spec.rb file & tests

---
+++ spec/problem25_spec.rb
@@ -0,0 +1,14 @@
+require 'problem25'
+
+describe 'Problem25' do
+
+  describe '1000-digit Fibonacci Number' do
+    it 'Works Out The 3-digit Fibonacci Number' do
+      expect(find_fibonacci_number(3)).to eq 12
+    end
+
+    it 'Works Out The 1000-digit Fibonacci Number' do
+      expect(find_fibonacci_number(1000)).to eq 648
+    end
+  end
+end
