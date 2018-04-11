70dad7bf9e38458022103cd1427ab745bae752d7
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-04-11 01:00:23 +0100

    Add problem1_spec.rb file and tests

---
+++ spec/problem1_spec.rb
@@ -0,0 +1,14 @@
+require 'problem1'
+
+describe 'Problem1' do
+
+  describe 'Multiples Of 3 And 5' do
+    it 'Works Out The Sum Of Multiples Of 3 And 5 Below 10' do
+      expect(multiples_of_three_and_five(10)).to eq 23
+    end
+
+    it 'Works Out The Sum Of Multiples Of 3 And 5 Below 100' do
+      expect(multiples_of_three_and_five(1000)).to eq 233168
+    end
+  end
+end
