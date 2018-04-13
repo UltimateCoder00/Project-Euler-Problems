e93055b5b97ceaa5db96750913955d1f1eb429ad
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-04-13 13:10:51 +0100

    Add problem47_spec.rb file & tests

---
+++ spec/problem47_spec.rb
@@ -0,0 +1,18 @@
+require 'problem47'
+
+describe 'Problem47' do
+
+  describe 'Distinct Primes Factors' do
+    it 'Works Out The Distinct Primes Factors Of Two Consecutive Numbers' do
+      expect(distinct_primes_factors(2)).to eq 14
+    end
+
+    it 'Works Out The Distinct Primes Factors Of Three Consecutive Numbers' do
+      expect(distinct_primes_factors(3)).to eq 644
+    end
+
+    it 'Works Out The Distinct Primes Factors Of Four Consecutive Numbers' do
+      expect(distinct_primes_factors(4)).to eq 644
+    end
+  end
+end
