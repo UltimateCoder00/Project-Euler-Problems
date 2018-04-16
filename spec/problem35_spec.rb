065ef23b4bccc315e44a8ff4016f34cf2d099fb5
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-04-16 13:56:00 +0100

    Add problem35_spec.rb File & Tests

---
+++ spec/problem35_spec.rb
@@ -0,0 +1,14 @@
+require 'problem35'
+
+describe 'Problem35' do
+
+  describe 'Circular Primes' do
+    it 'Works Out Distinct Number Of Circular Primes Below 100' do
+      expect(circular_primes(100)).to eq 13
+    end
+
+    it 'Works Out Distinct Number Of Circular Primes Below 1000000' do
+      expect(circular_primes(1000000)).to eq 55
+    end
+  end
+end
