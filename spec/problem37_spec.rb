b7c597a9882d82cdc968e8b08fed2c54b59bf1cf
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-04-16 14:27:31 +0100

    Add problem37_spec.rb File & Test

---
+++ spec/problem37_spec.rb
@@ -0,0 +1,10 @@
+require 'problem37'
+
+describe 'Problem37' do
+
+  describe 'Truncatable Primes' do
+    it 'Works Out The Sum Of Truncatable Primes' do
+      expect(truncatable_primes).to eq 748317
+    end
+  end
+end
