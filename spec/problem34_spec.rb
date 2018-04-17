3f756c705d189252325d68a28e7ed15d76581dbd
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-04-17 01:37:49 +0100

    Add problem34_spec.rb File & Test

---
+++ spec/problem34_spec.rb
@@ -0,0 +1,10 @@
+require 'problem34'
+
+describe 'Problem34' do
+
+  describe 'Digit Factorials' do
+    it 'Works Out The Digit Factorials' do
+      expect(digit_factorials).to eq 40730
+    end
+  end
+end
