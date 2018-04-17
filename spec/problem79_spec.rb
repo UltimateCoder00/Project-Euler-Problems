c15e5d557471ea8e93a8b2b4e182b9c6f54f47c6
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-04-17 15:17:19 +0100

    Add problem79_spec.rb File & Test

---
+++ spec/problem79_spec.rb
@@ -0,0 +1,10 @@
+require 'problem79'
+
+describe 'Problem79' do
+
+  describe 'Passcode Derivation' do
+    it 'Works Out The Passcode Derivation' do
+      expect(passcode_derivation).to eq 73162890
+    end
+  end
+end
