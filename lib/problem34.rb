3129954d3cff0ec51d53fa5a924b8c3064193c3f
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-04-17 01:38:47 +0100

    Add problem34.rb File & digit_factorials Method

---
+++ lib/problem34.rb
@@ -0,0 +1,22 @@
+def digit_factorials
+  sum = 0
+
+  for i in 10..100000
+    if i == curious_number(i)
+      sum += i
+    end
+  end
+
+  sum
+end
+
+def factorial(n)
+  return 1 if n < 2
+  n * factorial(n-1)
+end
+
+def curious_number(i)
+  i.to_s.split("").map! {|x| x = x.to_i}.inject(0) do |sum, x|
+    sum += factorial(x)
+  end
+end
