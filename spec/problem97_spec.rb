7d6bbbeddffa8ee700e43a15becad145f77e604e
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-04-18 14:15:15 +0100

    Add problem97_spec.rb File & Test

---
+++ spec/problem97_spec.rb
@@ -0,0 +1,10 @@
+require 'problem97'
+
+describe 'Problem97' do
+
+  describe 'Large Non-Mersenne Prime' do
+    it 'Works Out The Large Non-Mersenne Prime' do
+      expect(large_non_mersenne_prime).to eq 8739992577
+    end
+  end
+end
