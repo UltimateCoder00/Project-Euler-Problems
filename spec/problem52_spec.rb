867c8c2707c3370634108666f1b90d8de47e8ede
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-04-18 01:47:47 +0100

    Add problem52_spec.rb File & Test

---
+++ spec/problem52_spec.rb
@@ -0,0 +1,10 @@
+require 'problem52'
+
+describe 'Problem52' do
+
+  describe 'Permuted Multiples' do
+    it 'Works Out The Permuted Multiples' do
+      expect(permuted_multiples).to eq 142857
+    end
+  end
+end
