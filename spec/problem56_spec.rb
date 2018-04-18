7488027c54b9d62eb1b880ac80ca25a34851e7a1
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-04-18 02:42:10 +0100

    Add problem56.rb File & Test

---
+++ spec/problem56_spec.rb
@@ -0,0 +1,10 @@
+require 'problem56'
+
+describe 'Problem56' do
+
+  describe 'Powerful Digit Sum' do
+    it 'Works Out The Powerful Digit Sum' do
+      expect(powerful_digit_sum).to eq 972
+    end
+  end
+end
