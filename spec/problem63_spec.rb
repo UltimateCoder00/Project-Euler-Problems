86bf50287d31dba32a3350a3d9f2f93f829a8377
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-04-18 02:21:21 +0100

    Add problem63_spec.rb File & Test

---
+++ spec/problem63_spec.rb
@@ -0,0 +1,10 @@
+require 'problem63'
+
+describe 'Problem63' do
+
+  describe 'Powerful Digit Counts' do
+    it 'Works Out The Powerful Digit Counts' do
+      expect(powerful_digit_counts).to eq 49
+    end
+  end
+end
