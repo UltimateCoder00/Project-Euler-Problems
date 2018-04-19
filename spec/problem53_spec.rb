1d27594f72d6f0531a90c9d90a487e083514419b
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-04-19 16:17:16 +0100

    Add problem53_spec.rb File & Test

---
+++ spec/problem53_spec.rb
@@ -0,0 +1,10 @@
+require 'problem53'
+
+describe 'Problem53' do
+
+  describe 'Combinatoric Selections' do
+    it 'Works Out The Combinatoric Selections' do
+      expect(combinatoric_selections).to eq 4075
+    end
+  end
+end
