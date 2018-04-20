f310d06dc8b211435eea39b5347f07eb99621866
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-04-21 00:38:13 +0100

    Update tests

--- spec/problem12_spec.rb
+++ spec/problem12_spec.rb
@@ -4,11 +4,11 @@ describe 'Problem12' do
 
   describe 'Highly Divisible Triangular Number' do
     it 'Works Out The Highly Divisible Triangular Number Of More Than 5 Divisors' do
-      expect(highly_divisible_triangular_number(5)).to eq 28
+      expect(highly_divisible_triangular_number(5)).to eq 6
     end
 
     it 'Works Out The Highly Divisible Triangular Number Of More Than 500 Divisors' do
-      expect(highly_divisible_triangular_number(500)).to eq 28
+      expect(highly_divisible_triangular_number(500)).to eq 76576500
     end
   end
 end
