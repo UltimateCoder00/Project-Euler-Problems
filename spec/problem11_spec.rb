b872e8b04c3ba4b90bae5a14ff24529c94f4d478
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-04-23 12:08:44 +0100

    Add problem11_spec.rb File & Test

---
+++ spec/problem11_spec.rb
@@ -0,0 +1,10 @@
+require 'problem11'
+
+describe 'Problem11' do
+  
+  describe 'Largest Product In A Grid' do
+    it 'Works Out The Largest Product In A Grid' do
+      expect(largest_product_in_a_grid.to eq nil
+    end
+  end
+end
