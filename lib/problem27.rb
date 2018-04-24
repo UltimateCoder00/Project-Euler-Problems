389dcb4b77eaaf5b5fff14f273c3833ddd8158c6
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-04-25 00:04:44 +0100

    Refactor quadratic_primes Method

--- lib/problem27.rb
+++ lib/problem27.rb
@@ -4,9 +4,13 @@ def quadratic_primes
   product = 0
   primes = 0
 
-  (-999...1000).each do |a|
-    (-1000..1000).each do |b|
-      n = 0
+  a = (-999...1000).select { |x| x.odd? }
+  b = (-1000..1000).select { |x| Prime.prime?(x) }
+
+
+  a.each do |a|
+    b.each do |b|
+      n = 1
 
       while true
         Prime.prime?(n**2 + a*n + b) ? n += 1 : break
