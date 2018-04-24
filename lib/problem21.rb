9890195e9efc74060d8754beef48f2eb9f62ee48
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-04-24 13:35:27 +0100

    Refactor amicable_numbers Method

--- lib/problem21.rb
+++ lib/problem21.rb
@@ -1,31 +1,20 @@
 require 'prime'
 
 def amicable_numbers(number)
-  array = []
+  remove_primes(number).select do |i|
+    amicable?(i)
+  end.inject(:+)
+end
 
-  for i in 1...number
-    next if Prime.prime?(i)
-    next if array.include?(i)
+def remove_primes(number)
+  (1...number).reject { |x| Prime.prime?(x) }
+end
 
-    factor_sum = factor_sum(i)
-
-    if factor_sum(factor_sum) == i
-      unless factor_sum == i
-        array << i
-        array << factor_sum
-      end
-    end
-  end
-
-  array.inject(:+)
+def amicable?(i)
+  sum = factor_sum(i)
+  factor_sum(sum) == i && sum != i
 end
 
 def factor_sum(x)
-  sum = 0
-
-  for i in 1..(x/2)
-    sum += i if (x % i).zero?
-  end
-
-  sum
+  (1..(x/2)).inject(0) { |count, i| (x % i).zero? ? count + i : count }
 end
