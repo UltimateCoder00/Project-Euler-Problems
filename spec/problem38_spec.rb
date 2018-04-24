adacc9b450688e05e19c5b74baa5be2369ef239a
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-04-25 00:46:56 +0100

    Add problem38_spec.rb & Test

---
+++ spec/problem38_spec.rb
@@ -0,0 +1,10 @@
+require 'problem38'
+
+describe 'Problem38' do
+
+  describe 'Pandigital Multiples' do
+    it 'Works Out The Pandigital Multiples' do
+      expect(pandigital_multiples).to eq 932718654
+    end
+  end
+end
