92262f069a7b62db9cfac6c803bbcdb4f842d2fb
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-04-24 22:48:15 +0100

    Add problem27_spec.rb File & Test

---
+++ spec/problem27_spec.rb
@@ -0,0 +1,10 @@
+require 'problem27'
+
+describe 'Problem27' do
+
+  describe 'Quadratic Primes' do
+    it 'Works Out The Quaratic Primes' do
+      expect(quadratic_primes).to eq -59231
+    end
+  end
+end
