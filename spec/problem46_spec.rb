9d33afb86beca6b43b29451af094ae166e4ac3db
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-04-25 09:39:55 +0100

    Add problem46_spec.rb File & Test

---
+++ spec/problem46_spec.rb
@@ -0,0 +1,10 @@
+require 'problem46'
+
+describe 'Problem46' do
+
+  describe 'Goldbachs Other Conjecture' do
+    it 'Works Out The Goldbachs Other Conjecture' do
+      expect(goldbachs_other_conjecture).to eq 5777
+    end
+  end
+end
