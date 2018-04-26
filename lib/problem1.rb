79d44a5d220bcef91a1df41dbdff123ff0f6b7c3
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-04-26 18:50:28 +0100

    Refactor

--- lib/problem1.rb
+++ lib/problem1.rb
@@ -1,3 +1,17 @@
-def multiples_of_three_and_five(number)
-  (1...number).inject(0) { |sum, i| (i % 3).zero? || (i % 5).zero? ? sum + i : sum }
+def multiples_of_three_and_five(limit)
+  (1...limit).inject(0) do |sum, natural_number|
+    divisible_by_three_and_five?(natural_number) ? sum + natural_number : sum
+  end
+end
+
+def divisible_by_three_and_five?(natural_number)
+  divisible_by_three?(natural_number) || divisible_by_five?(natural_number)
+end
+
+def divisible_by_three?(natural_number)
+  (natural_number % 3).zero?
+end
+
+def divisible_by_five?(natural_number)
+  (natural_number % 5).zero?
 end
