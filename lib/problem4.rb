f5a645bcaba1ff71a66a1bfdbc2a99b54cc91517
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-04-26 23:48:28 +0100

    Refactor

--- lib/problem4.rb
+++ lib/problem4.rb
@@ -2,14 +2,14 @@ require 'common'
 
 def largest_palindrome_product(digit_limit)
   limit = ("9"*digit_limit).to_i
-  palindromic_number = 0
+  largest_palindromic_number = 0
 
   (limit).downto(1) do |a|
-    return palindromic_number if a*limit < palindromic_number
+    return largest_palindromic_number if a*limit < largest_palindromic_number
     (limit).downto(1) do |b|
       product = a*b
-      break if product < palindromic_number
-      palindromic_number = [palindromic_number, product].max if palindromic?(product)
+      break if product < largest_palindromic_number
+      largest_palindromic_number = [largest_palindromic_number, product].max if palindromic?(product)
     end
   end
 end
