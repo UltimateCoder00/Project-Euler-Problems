136878f6a950ab6cb79e31e45884ce9dc3655094
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-04-26 23:46:47 +0100

    Refactor

--- lib/problem1.rb
+++ lib/problem1.rb
@@ -1,3 +1,5 @@
+require_relative 'common'
+
 def multiples_of_three_and_five(limit)
   (1...limit).inject(0) do |sum, natural_number|
     divisible_by_three_and_five?(natural_number) ? sum + natural_number : sum
@@ -5,13 +7,5 @@ def multiples_of_three_and_five(limit)
 end
 
 def divisible_by_three_and_five?(natural_number)
-  divisible_by_three?(natural_number) || divisible_by_five?(natural_number)
-end
-
-def divisible_by_three?(natural_number)
-  (natural_number % 3).zero?
-end
-
-def divisible_by_five?(natural_number)
-  (natural_number % 5).zero?
+  divisible?(natural_number, 3) || divisible?(natural_number, 5)
 end
