06304f16286cebfb6181c5f1aaed09ab0b92050c
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-04-26 19:54:48 +0100

    Refactor

--- lib/problem2.rb
+++ lib/problem2.rb
@@ -1,16 +1,36 @@
-def even_fibonacci_numbers(number)
-  sum = 2
-
-  a = 1
-  b = 2
-  c = a + b
-
-  while c <= number
-    a = b
-    b = c
-    c = a + b
-    sum += c if (c % 2).zero?
+def even_fibonacci_numbers(limit)
+  loop do
+    return sum_of_even_fibonacci_terms if last_fibonacci_term_beyond?(limit)
+    calculate_next_fibonacci_term
+    add_to_sum_of_even_fibonacci_terms if last_fibonacci_term_even?
   end
+end
 
-  sum
+def sum_of_even_fibonacci_terms
+  fibonacci_array.last
+end
+
+def fibonacci_array
+  @fibonacci_array ||= [[1, 2], 2]
+end
+
+def last_fibonacci_term_beyond?(limit)
+  last_two_fibonacci_terms.last > limit
+end
+
+def last_two_fibonacci_terms
+  fibonacci_array.first
+end
+
+def calculate_next_fibonacci_term
+  last_two_fibonacci_terms.reverse!
+  last_two_fibonacci_terms[-1] += last_two_fibonacci_terms.first
+end
+
+def add_to_sum_of_even_fibonacci_terms
+  fibonacci_array[1] += last_two_fibonacci_terms.last
+end
+
+def last_fibonacci_term_even?
+  last_two_fibonacci_terms.last.even?
 end
