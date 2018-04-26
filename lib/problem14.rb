e137b3fd43c6c8f5e8524dc8e9b5e8b47c4446d6
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-04-26 14:59:55 +0100

    Refactor longest_collatz_sequence Method

--- lib/problem14.rb
+++ lib/problem14.rb
@@ -2,17 +2,22 @@ def longest_collatz_sequence
   count = 0
   starting_series = 2
 
-  for i in 2...1000000
-    temp_count = sequence(1 , i)
-    starting_series = i if temp_count > count
-    count = [count, temp_count].max
+  for i in 2...1_000_000
+    array[i] = sequence(1, i)
+    starting_series = i if array[i] > count
+    count = [count, array[i]].max
   end
 
   starting_series
 end
 
+def array
+  @array ||= [0, 1]
+end
+
 def sequence(count , n)
   return count if n == 1
+  return count + array[n] unless array[n] == nil
   n.even? ? sequence(count + 1, even(n)) : sequence(count + 1, odd(n))
 end
 
