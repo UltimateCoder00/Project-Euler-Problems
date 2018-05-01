b669f2ce0eab711e3729f6a2c2cc2aa47978f958
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-05-01 18:01:23 +0100

    Refactor

--- lib/problem6.rb
+++ lib/problem6.rb
@@ -1,13 +1,11 @@
+require 'common'
+
 def sum_square_difference(limit)
-  sum_squared(limit) - squared_sum(limit)
+  sum_squared(limit) - array_squared_sum(range_array(limit))
 end
 
 def sum_squared(limit)
   range_array(limit).sum**2
-end
-
-def squared_sum(limit)
-  range_array(limit).map! { |x| x**2 }.sum
 end
 
 def range_array(limit)
