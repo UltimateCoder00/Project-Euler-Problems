cb47596ae818d284e46267bc064036d4a20d2a26
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-05-01 18:00:55 +0100

    Refactor

--- lib/problem9.rb
+++ lib/problem9.rb
@@ -1,16 +1,17 @@
-def special_pythagorean_triplet(number)
-  for a in 1..number
-    for b in 1..number
-      break if a + b > number
+require 'common'
 
-      c_2 = a**2 + b**2
-      c = Math.sqrt(c_2).to_i
-      if c_2 == c**2
-        if a + b + c == number
-          return a*b*c
-        end
-      end
+def special_pythagorean_triplet(limit)
+  (1..limit).each do |a|
+    (1..limit).each do |b|
+      break if b > limit - a - b
+      c_2 = squared_sum(a, b)
+      next unless square_root_natural_number?(c_2)
+      c = integer_square_root(c_2)
+      return a*b*c if a + b + c == limit
     end
   end
+end
 
+def square_root_natural_number?(natural_number)
+  integer_square_root(natural_number)**2 == natural_number
 end
