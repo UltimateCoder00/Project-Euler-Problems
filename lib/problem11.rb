bbe1d306be4b935d90148aa713d06e295985c751
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-05-01 21:17:25 +0100

    Refactor

--- lib/problem11.rb
+++ lib/problem11.rb
@@ -1,29 +1,45 @@
+require 'common'
+
 def largest_product_in_a_grid(array)
-  max_product = 0
+  horizontal_array = array
+  vertical_array = array.transpose
+  diagonal_array1 = diagonal_array(array)
+  diagonal_array2 = diagonal_array(array.reverse).transpose
 
-  for i in 0...array.length
-    for j in 0...(array.length-3)
-      max_product = [max_product,array[i][j]*array[i][j+1]*array[i][j+2]*array[i][j+3]].max
-    end
+  four_digit_max_product = 0
+
+  four_digit_max_product = max_product(four_digit_max_product, horizontal_array)
+  four_digit_max_product = max_product(four_digit_max_product, vertical_array)
+  four_digit_max_product = max_product(four_digit_max_product, diagonal_array1)
+  four_digit_max_product = max_product(four_digit_max_product, diagonal_array2)
+
+  four_digit_max_product
+end
+
+def max_product(four_digit_max_product, array)
+  array.each do |x|
+    four_digit_max_product = [four_digit_max_product, four_digit_max_product(x.dup)].max
+  end
+  four_digit_max_product
+end
+
+def four_digit_max_product(natural_number_array)
+  horizontal_product = 0
+  loop do
+    break if natural_number_array.length < 4
+    horizontal_product = [horizontal_product, digit_product(natural_number_array.last(4))].max
+    natural_number_array.pop
   end
 
-  for i in 0...(array.length-3)
-    for j in 0...array.length
-      max_product = [max_product,array[i][j]*array[i+1][j]*array[i+2][j]*array[i+3][j]].max
-    end
+  horizontal_product
+end
+
+def diagonal_array(array)
+  new_array = []
+
+  array.each_with_index do |array, index|
+    new_array << array.rotate(index)
   end
 
-  for i in 0...(array.length-3)
-    for j in 0...(array.length-3)
-      max_product = [max_product,array[i][j]*array[i+1][j+1]*array[i+2][j+2]*array[i+3][j+3]].max
-    end
-  end
-
-  for i in 3...array.length
-    for j in 0...(array.length-3)
-      max_product = [max_product,array[i][j]*array[i-1][j+1]*array[i-2][j+2]*array[i-3][j+3]].max
-    end
-  end
-
-  max_product
+  new_array
 end
