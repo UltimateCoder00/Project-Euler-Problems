2cc51dccb9aba28db7e9ada1c74d215dd0041916
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-05-02 20:07:06 +0100

    Refactor

--- spec/problem4_spec.rb
+++ spec/problem4_spec.rb
@@ -4,11 +4,11 @@ describe 'Problem4' do
 
   describe 'Largest Palindrome Product' do
     it 'Works Out The Largest Palindrome Product Of Two 2-Digit Numbers' do
-      expect(largest_palindrome_product(2)).to eq 9009
+      expect(largest_palindrome_product(2)).to eq 9_009
     end
 
     it 'Works Out The Largest Palindrome Product Of Two 3-Digit Numbers' do
-      expect(largest_palindrome_product(3)).to eq 906609
+      expect(largest_palindrome_product(3)).to eq 906_609
     end
   end
 end
