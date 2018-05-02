e6cd0c714b218e846e0d2a323967ecaa0ceffc6c
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-05-02 20:07:36 +0100

    Refactor

--- spec/problem9_spec.rb
+++ spec/problem9_spec.rb
@@ -4,7 +4,7 @@ describe 'Problem9' do
 
   describe 'Special Pythagorean Triplet' do
     it 'Works Out Special Pythagorean Triplet Of a+b+c=1000' do
-      expect(special_pythagorean_triplet(1000)).to eq 31875000
+      expect(special_pythagorean_triplet(1_000)).to eq 31_875_000
     end
   end
 end
