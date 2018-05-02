e9bf6a0c6299f0895c495f32caeff57d11c05c6f
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-05-02 20:07:53 +0100

    Refactor

--- spec/problem12_spec.rb
+++ spec/problem12_spec.rb
@@ -8,7 +8,7 @@ describe 'Problem12' do
     end
 
     it 'Works Out The Highly Divisible Triangular Number Of More Than 500 Divisors' do
-      expect(highly_divisible_triangular_number(500)).to eq 76576500
+      expect(highly_divisible_triangular_number(500)).to eq 76_576_500
     end
   end
 end
