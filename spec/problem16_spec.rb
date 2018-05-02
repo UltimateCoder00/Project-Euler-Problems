349e415abae1db74cdc7a650fa94eabe0339149e
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-05-02 20:08:16 +0100

    Refactor

--- spec/problem16_spec.rb
+++ spec/problem16_spec.rb
@@ -8,7 +8,7 @@ describe 'Problem16' do
     end
 
     it 'Works Out The Power Digit Sum Of 2**1000' do
-      expect(power_digit_sum(1000)).to eq 1366
+      expect(power_digit_sum(1_000)).to eq 1_366
     end
   end
 end
