9d2cca109e119d64d1ec634fbfa6c3041f3713c4
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-05-02 20:08:20 +0100

    Refactor

--- spec/problem17_spec.rb
+++ spec/problem17_spec.rb
@@ -8,7 +8,7 @@ describe 'Problem17' do
     end
 
     it 'Works Out The Number Letter Counts Of 1 To 1000' do
-      expect(number_letter_counts(1000)).to eq 21124
+      expect(number_letter_counts(1_000)).to eq 21_124
     end
   end
 end
