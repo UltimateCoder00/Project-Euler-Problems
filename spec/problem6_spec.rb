677f64d36291cb740b9c696e84de1ad00430a178
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-05-02 20:07:15 +0100

    Refactor

--- spec/problem6_spec.rb
+++ spec/problem6_spec.rb
@@ -4,11 +4,11 @@ describe 'Problem6' do
 
   describe 'Sum Square Difference' do
     it 'Works Out The Sum Square Difference Of 10' do
-      expect(sum_square_difference(10)).to eq 2640
+      expect(sum_square_difference(10)).to eq 2_640
     end
 
     it 'Works Out The Sum Square Difference Of 100' do
-      expect(sum_square_difference(100)).to eq 25164150
+      expect(sum_square_difference(100)).to eq 25_164_150
     end
   end
 end
