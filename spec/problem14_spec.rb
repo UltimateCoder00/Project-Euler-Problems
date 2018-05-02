129cbadfdd137dab42685dc6e1e8bfa8c3b86567
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-05-02 20:08:07 +0100

    Refactor

--- spec/problem14_spec.rb
+++ spec/problem14_spec.rb
@@ -4,7 +4,7 @@ describe 'Problem14' do
 
   describe 'Longest Collatz Sequence' do
     it 'Works Out The Longest Collatz Sequence Below 1 Million' do
-      expect(longest_collatz_sequence).to eq 837799
+      expect(longest_collatz_sequence(1_000_000)).to eq 837_799
     end
   end
 end
