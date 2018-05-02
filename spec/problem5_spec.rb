99a8eac98b34aeec8912c75ff55dd88dedc26328
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-05-02 20:07:10 +0100

    Refactor

--- spec/problem5_spec.rb
+++ spec/problem5_spec.rb
@@ -4,11 +4,11 @@ describe 'Problem5' do
 
   describe 'Smallest Multiple' do
     it 'Works Out The Smallest Multiple From 1 To 10' do
-      expect(smallest_multiple(10)).to eq 2520
+      expect(smallest_multiple(10)).to eq 2_520
     end
 
     it 'Works Out The Smallest Multiple From 1 To 20' do
-      expect(smallest_multiple(20)).to eq 232792560
+      expect(smallest_multiple(20)).to eq 232_792_560
     end
   end
 end
