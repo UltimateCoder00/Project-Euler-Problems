3559a7b39a7c564e2d5dfac50d770944c0c901d8
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-05-02 20:07:00 +0100

    Refactor

--- spec/problem3_spec.rb
+++ spec/problem3_spec.rb
@@ -4,11 +4,11 @@ describe 'Problem3' do
 
   describe 'Largest Prime Factor' do
     it 'Works Out The Largest Prime Factor Of 13195' do
-      expect(largest_prime_factor(13195)).to eq 29
+      expect(largest_prime_factor(13_195)).to eq 29
     end
 
     it 'Works Out The Largest Prime Factor Of 600851475143' do
-      expect(largest_prime_factor(600851475143)).to eq 6857
+      expect(largest_prime_factor(600_851_475_143)).to eq 6_857
     end
   end
 end
