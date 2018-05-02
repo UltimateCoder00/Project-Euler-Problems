f493a915c093361be3ba3282db415519db6e3385
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-05-02 20:06:55 +0100

    Refactor

--- spec/problem2_spec.rb
+++ spec/problem2_spec.rb
@@ -4,7 +4,7 @@ describe 'Problem2' do
 
   describe 'Even Fibonacci Numbers' do
     it 'Works Out The Even Fibonacci Numbers Below Four Million' do
-      expect(even_fibonacci_numbers(4000000)).to eq 4613732
+      expect(even_fibonacci_numbers(4_000_000)).to eq 4_613_732
     end
   end
 end
