1973b0db199d90c420f0082108d4930395bbf3e6
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-05-03 14:20:15 +0100

    Refactor

--- lib/problem17.rb
+++ lib/problem17.rb
@@ -1,7 +1,7 @@
 require 'humanize'
 
-def number_letter_counts(number)
-  (1..number).inject(0) do |sum, number|
+def number_letter_counts(limit)
+  (1..limit).inject(0) do |sum, number|
     sum += number.humanize.split("").reject do |char|
       (char == "-") || (char == " ")
     end.count
