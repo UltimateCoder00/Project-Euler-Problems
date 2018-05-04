9ca63bd9648c7376cc66ba687f7138700959af13
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-05-04 02:05:56 +0100

    Add problem31 File & Test

---
+++ spec/problem31_spec.rb
@@ -0,0 +1,10 @@
+require 'problem31'
+
+describe 'Problem31' do
+
+  describe 'Coin Sums' do
+    it 'Works Out The Coin Sums' do
+      expect(coin_sums([1,2,5,10,20,50,100,200], 200)).to eq 73682
+    end
+  end
+end
