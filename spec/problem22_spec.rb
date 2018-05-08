1dc58cc71fcb0501b509f1d2165c0d17ebc7feb8
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-05-08 23:24:59 +0100

    Add problem22_spec.rb File & Test

---
+++ spec/problem22_spec.rb
@@ -0,0 +1,10 @@
+require 'problem22'
+
+describe 'Problem22' do
+
+  describe 'Names Scores' do
+    it 'Works Out The Names Scores' do
+      expect(names_scores).to eq 871198282
+    end
+  end
+end
