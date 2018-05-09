0e88f7d9f846c069055d6cc652fc747ecdda29c1
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-05-09 13:45:22 +0100

    Add problem33_spec.rb File & Test

---
+++ spec/problem33_spec.rb
@@ -0,0 +1,10 @@
+require 'problem33'
+
+describe 'Problem33' do
+
+  describe 'Digit Cancelling Fractions' do
+    it 'Works Out The Digit Cancelling Fractions' do
+      expect(digit_cancelling_fractions).to eq 100
+    end
+  end
+end
