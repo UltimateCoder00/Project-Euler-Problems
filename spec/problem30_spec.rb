17ad556a01b2281802339810942c8121dab1c3e7
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-05-10 13:22:20 +0100

    Add problem30_spec.rb File And Test

---
+++ spec/problem30_spec.rb
@@ -0,0 +1,10 @@
+require 'problem30'
+
+describe 'Problem30' do
+
+  describe 'Digit Fifth Powers' do
+    it 'Works Out The Digit Fifth Powers' do
+      expect(digit_fifth_powers).to eq nil
+    end
+  end
+end
