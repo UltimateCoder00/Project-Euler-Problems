43e8f1f6975f967f0ea8ff429e3558f6f9b31474
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-05-13 01:28:10 +0100

    Add problem43_spec.rb File & Test

---
+++ spec/problem43_spec.rb
@@ -0,0 +1,10 @@
+require 'problem43'
+
+describe 'Problem43' do
+
+  describe 'Sub-String Divisibilty' do
+    it 'Works Out The Sub-String Divisibilty' do
+      expect(substring_divisibility).to eq 16695334890
+    end
+  end
+end
