340d678fb8e5a7dce1d86a44e293b99f923d45e6
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-05-13 02:14:06 +0100

    Add problem41_spec.rb File & Test

---
+++ spec/problem41_spec.rb
@@ -0,0 +1,10 @@
+require 'problem41'
+
+describe 'Problem41' do
+
+  describe 'Pandigital Prime' do
+    it 'Works Out The Pandigital Prime' do
+      expect(pandigital_prime).to eq 7652413
+    end
+  end
+end
