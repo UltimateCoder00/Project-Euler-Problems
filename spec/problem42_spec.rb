7fcc12a639a4df8607dee6f6d26077e0c599a5d2
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-05-13 13:15:01 +0100

    Add problem42_spec.rb File And Test

---
+++ spec/problem42_spec.rb
@@ -0,0 +1,10 @@
+require 'problem42'
+
+describe 'Problem42' do
+
+  describe 'Coded Triangle Numbers' do
+    it 'Works Out The Coded Triangle Numbers' do
+      expect(coded_triangle_numbers).to eq nil
+    end
+  end
+end
