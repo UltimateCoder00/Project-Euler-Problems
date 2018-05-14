7e49a7291c1f298c97ff05efdccbb95c1ba367ef
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-05-14 12:04:44 +0100

    Add problem45_spec.rb File & Test

---
+++ spec/problem45_spec.rb
@@ -0,0 +1,10 @@
+require 'problem45'
+
+describe 'Problem45' do
+
+  describe 'Triangular, Pentagonal, and Hexagonal' do
+    it 'Works Out The Triangular, Pentagonal, and Hexagonal' do
+      expect(triangular_pentagonal_and_hexagonal).to eq 1533776805
+    end
+  end
+end
