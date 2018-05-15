ea11a09b89ade9e771543c10d86110715ba56995
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-05-15 15:57:08 +0100

    Add problem40_spec.rb File & Test

---
+++ spec/problem40_spec.rb
@@ -0,0 +1,10 @@
+require 'problem40'
+
+describe 'Problem40' do
+
+  describe "Champernowne's Constant" do
+    it "Works Out The Champernowne's Constant" do
+      expect(champernownes_constant).to eq 210
+    end
+  end
+end
