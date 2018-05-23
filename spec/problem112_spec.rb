2bd764a538dc6021a54b20e88294ebc35082561e
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-05-23 18:27:44 +0100

    Add problem112_spec.rb File & Test

---
+++ spec/problem112_spec.rb
@@ -0,0 +1,9 @@
+require 'problem112'
+
+describe 'Problem112' do
+  describe 'Bouncy Numbers' do
+    it 'Works Out The Bouncy Numbers' do
+      expect(bouncy_numbers).to eq 1_587_000
+    end
+  end
+end
