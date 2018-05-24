5f8f27d59f872f6342403015aa7e509ecf00b534
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-05-24 21:18:33 +0100

    Add problem23_spec.rb File & Test

---
+++ spec/problem23_spec.rb
@@ -0,0 +1,9 @@
+require 'problem23'
+
+describe 'Problem23' do
+  describe 'Non-Abundant Sums' do
+    it 'Works Out The Non-Abundant Sums' do
+      expect(nonabundant_sums).to eq 4_179_871
+    end
+  end
+end
