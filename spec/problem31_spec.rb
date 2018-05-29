71026aed4605f7a3843d5df8f9651a68cca0683c
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-05-29 22:13:20 +0100

    Refactor

--- spec/problem31_spec.rb
+++ spec/problem31_spec.rb
@@ -1,10 +1,9 @@
 require 'problem31'
 
 describe 'Problem31' do
-
   describe 'Coin Sums' do
     it 'Works Out The Coin Sums' do
-      expect(coin_sums([1,2,5,10,20,50,100,200], 200)).to eq 73682
+      expect(coin_sums([1, 2, 5, 10, 20, 50, 100, 200], 200)).to eq 73_682
     end
   end
 end
