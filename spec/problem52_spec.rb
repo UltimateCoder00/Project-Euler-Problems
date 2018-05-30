b05dbb979908471b7ee8915ae35da9b21d522518
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-05-30 15:16:40 +0100

    Refactor

--- spec/problem52_spec.rb
+++ spec/problem52_spec.rb
@@ -1,10 +1,9 @@
 require 'problem52'
 
 describe 'Problem52' do
-
   describe 'Permuted Multiples' do
     it 'Works Out The Permuted Multiples' do
-      expect(permuted_multiples).to eq 142857
+      expect(permuted_multiples).to eq 142_857
     end
   end
 end
