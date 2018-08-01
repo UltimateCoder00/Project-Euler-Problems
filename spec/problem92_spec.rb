406d44da545e950b52a9aa5fd39dd5b89c278073
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-08-01 21:12:11 +0100

    Refactor

--- spec/problem92_spec.rb
+++ spec/problem92_spec.rb
@@ -3,7 +3,7 @@ require 'problem92'
 describe 'Problem92' do
   describe 'Square Digit Chains' do
     it 'Works Out The Square Digit Chains' do
-      expect(square_digit_chains(10_000_000)).to eq nil
+      expect(square_digit_chains(10_000_000)).to eq 8_581_146
     end
   end
 end
