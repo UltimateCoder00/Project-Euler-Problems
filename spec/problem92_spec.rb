57654ce35190ead5ba0d6fdcf32f1f0b678d148a
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-08-01 21:06:21 +0100

    Add problem92_spec.rb File & Test

---
+++ spec/problem92_spec.rb
@@ -0,0 +1,9 @@
+require 'problem92'
+
+describe 'Problem92' do
+  describe 'Square Digit Chains' do
+    it 'Works Out The Square Digit Chains' do
+      expect(square_digit_chains(10_000_000)).to eq nil
+    end
+  end
+end
